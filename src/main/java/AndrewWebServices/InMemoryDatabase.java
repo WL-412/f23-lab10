5fdadbab5925468cb70c69b40dead37a3d064949
Wenhan Li <wenhan@wenhans-air.wifi.local.cmu.edu>
2023-11-26 18:19:58 -0500

    test fake stubs and mocks

--- src/main/java/AndrewWebServices/InMemoryDatabase.java
+++ src/main/java/AndrewWebServices/InMemoryDatabase.java
@@ -1,4 +1,6 @@
 package AndrewWebServices;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * InMemoryDatabase is a fake for the AndrewWS database which is used to improve test efficiency.
@@ -7,6 +9,14 @@ package AndrewWebServices;
  * 
  * Hint: there are two methods you need to implement
  */
-public class InMemoryDatabase /* should there be something here? */ {
-    // Implement your fake database here
+public class InMemoryDatabase extends Database /* should there be something here? */ {
+    private final Map<String, Integer> accounts = new HashMap<>();
+
+    public void addAccount(String accountName, int password) {
+        accounts.put(accountName, password);
+    }
+
+    public int getPassword(String accountName) {
+        return accounts.getOrDefault(accountName, -1); 
+    }
 }
--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -2,12 +2,16 @@ package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AndrewWebServicesTest {
-    Database database;
+    InMemoryDatabase fakeDatabase;
     RecSys recommender;
     PromoService promoService;
     AndrewWebServices andrewWebService;
@@ -15,34 +19,50 @@ public class AndrewWebServicesTest {
     @Before
     public void setUp() {
         // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+        MockitoAnnotations.initMocks(this);
+        fakeDatabase = new InMemoryDatabase(); // We probably don't want to access our real database...
+        fakeDatabase.addAccount("user1", 1234);
+        fakeDatabase.addAccount("user2", 5678);
 
-        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        // recommender = new RecSys();
+        recommender = mock(RecSys.class);
+        when(recommender.getRecommendation("user1")).thenReturn("hi");
+        when(recommender.getRecommendation("user2")).thenReturn("hihi");
+
+        promoService = mock(PromoService.class);
+
+        andrewWebService = new AndrewWebServices(fakeDatabase, recommender, promoService);
     }
 
     @Test
     public void testLogIn() {
         // This is taking way too long to test
-        assertTrue(andrewWebService.logIn("Scotty", 17214));
+        // assertTrue(andrewWebService.logIn("Scotty", 17214));
+        assertTrue(andrewWebService.logIn("user1", 1234));
+        assertFalse(andrewWebService.logIn("user1", 0000));
+
     }
 
     @Test
     public void testGetRecommendation() {
         // This is taking way too long to test
-        assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        assertEquals("hi", andrewWebService.getRecommendation("user1"));
+        assertEquals("hihi", andrewWebService.getRecommendation("user2"));
     }
 
     @Test
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.sendPromoEmail("test@example.com");
+        verify(promoService).mailTo("test@example.com");
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.logIn("user1", 1234);
+        verify(promoService, never()).mailTo(anyString());
     }
 }
